55612b5bbfee639c88762df4eb1c885e4c393d69
JREMbalmo <balmori.joshua.rem@gmail.com>
2026-08-22 22:47:57 +0800

    Removed blog section

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -73,7 +73,6 @@ const config: Config = {
           position: 'left',
           label: 'Tutorial',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
@@ -113,10 +112,6 @@ const config: Config = {
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
             {
               label: 'GitHub',
               href: 'https://github.com/facebook/docusaurus',
